278200268a9de23715d8758bfb3a1f84a4d982d3
Richard A. Rigaud <107000837+RichRas45s@users.noreply.github.com>
2024-03-27 20:52:48 -0400

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const StylesLoader = require('styles-loader')
+const stylesLoader = new StylesLoader()
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const { EnvironmentPlugin } = require('webpack');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+     output: {
+         libraryTarget: 'var',
+         library: 'Client'
+     },
+     optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
+    module: {
+        rules: [
+            {
+                test:  /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW(),
+        new EnvironmentPlugin({
+            NODE_ENV: 'production'
+          })
+    ]
+}
+
